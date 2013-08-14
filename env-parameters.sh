30b082d1dadd5fca334b27e2929bc56b1464d7b8
J. Lucas Grillo <jlucas.gl@gmail.com>
2013-08-14 13:18:28 +0100

    minnor changes in env

--- env-parameters.sh
+++ env-parameters.sh
@@ -2,8 +2,14 @@
 #          ACCULL ENV VARIABLES
 #############################################
 
-# accULL base
-export ACCULLBASE=$(pwd)/
+
+# accULL & CUDA directories
+export ACCULLBASE=$(pwd)
+
+export CUDADIR=/opt/soft/cuda/5.0/
+
+# ---- Do not edit below this line -----
+
 
 # yacf directory
 export YACFDIR=$ACCULLBASE/yacf/
@@ -14,16 +20,10 @@ export FRANGOLLODIR=$ACCULLBASE/frangollo
 # PYTHONPATH
 export PYTHONPATH=$YACFDIR:$PYTHONPATH
 
-# Defaults to NVIDIA Cuda setup - edit accordingly for other platforms
 # CUDA and OpenCL PATH
-export PATH=/usr/local/cuda/bin/:$PATH
-export CUDADIR=/usr/local/cuda
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/lib:$LD_LIBRARY_PATH
-export CPPFLAGS="-I/usr/local/cuda/include":$CPPFLAGS
-export LDFLAGS="-L/usr/local/cuda/lib":$LDFLAGS
 
-echo " Ensure you have edited the previous values according to your platform "
-echo " If your platform does not support CUDA , remove the --enable-cuda option "
-echo " from the configure line in the build-accull.sh script "
-echo " Remove the exit of the line below in this env script after you have setup the parameters "
-exit
+export PATH=$CUDADIR/bin/:$PATH
+export LD_LIBRARY_PATH=$CUDADIR/lib64:$CUDADIR/lib:/opt/soft/cuda/5.0/lib64:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=$CUDADIR'/include/':$FRANGOLLODIR'/src/interface/':$C_INCLUDE_PATH
+export CPPFLAGS="-I$CUDADIR/include "$CPPFLAGS
+export LDFLAGS="-L$CUDADIR/lib "$LDFLAGS
